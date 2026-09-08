bc5f8f5775ea17cbb27a170ea6cfc8750ba05606
Brian <brianmkandla73@gmail.com>
2026-09-08 19:19:58 +0200

    Add pantry item model and SQLite database

---
+++ app/src/main/java/com/brian/smartpantry/database/DatabaseHelper.java
@@ -0,0 +1,34 @@
+package com.brian.smartpantry.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+	
+	private static final String DATABASE_NAME = "smart_pantry.db";
+	private static final int DATABASE_VERSION = 1;
+	
+	public DatabaseHelper(Context context) 
+	{
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+	}
+	
+	@Override
+	public void onCreate(SQLiteDatabase db)
+	{
+		db.execSQL("CREATE TABLE pantry_items, " + 
+				"id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+				"name TEXT NOT NULL, " + 
+				"quantity REAL NOT NULL, " +
+				"unit TEXT NOT NULL, " + 
+				"expiry_date TEXT");
+	}
+	
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+	{
+		db.execSQL("DROP TABLE IF EXISTS pantry_items");
+		onCreate(db);
+	}
+}
